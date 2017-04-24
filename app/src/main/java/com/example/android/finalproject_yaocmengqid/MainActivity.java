636c6c6b3de0a5586dd2248e5338bafe84c0dcce
Maureen_Ding <md3474@tc.columbia.edu>
2017-04-24 16:51:24 -0400

    Build the initial recycle view for the MainActivity

--- app/src/main/java/com/example/android/finalproject_yaocmengqid/MainActivity.java
+++ app/src/main/java/com/example/android/finalproject_yaocmengqid/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.android.finalproject_yaocmengqid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
-        Intent intent = new Intent(this, RecordActivity.class);
-        startActivity(intent);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MainAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
+
