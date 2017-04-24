b210d1cf5c0b21650decb62b411992347022bbe6
yaochen1224 <yao.chen1224@foxmail.com>
2017-04-24 14:31:36 -0400

    RecordActivity and calendar

---
+++ app/src/main/java/com/example/android/finalproject_yaocmengqid/RecordActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.finalproject_yaocmengqid;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+
+public class RecordActivity extends AppCompatActivity
+    implements DatePickerDialog.OnDateSetListener {
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+
+        datePickerDialog = new DatePickerDialog(this, RecordActivity.this, 1970, 1, 1);
+    }
+
+    public void showDatePickerDialog(View view) {
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+}
