e740a9ecff13b7276c0f6c86a08823384182ac1c
yaochen1224 <yao.chen1224@foxmail.com>
2017-04-24 15:23:31 -0400

    Expense class added

---
+++ app/src/main/java/com/example/android/finalproject_yaocmengqid/Expense.java
@@ -0,0 +1,67 @@
+package com.example.android.finalproject_yaocmengqid;
+
+import java.util.ArrayList;
+
+/**
+ * Created by YaoChen on 4/24/17.
+ */
+
+public class Expense {
+    double amount;
+    String date;
+    String expenseType;
+    ArrayList<People> payers;
+    ArrayList<People> members;
+
+    public Expense(double amount, String date, String expenseType, ArrayList<People> payers, ArrayList<People> members) {
+        this.amount = amount;
+        this.date = date;
+        this.expenseType = expenseType;
+        this.payers = payers;
+        this.members = members;
+    }
+
+    public Expense() {
+
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getExpenseType() {
+        return expenseType;
+    }
+
+    public void setExpenseType(String expenseType) {
+        this.expenseType = expenseType;
+    }
+
+    public ArrayList<People> getPayers() {
+        return payers;
+    }
+
+    public void setPayers(ArrayList<People> payers) {
+        this.payers = payers;
+    }
+
+    public ArrayList<People> getMembers() {
+        return members;
+    }
+
+    public void setMembers(ArrayList<People> members) {
+        this.members = members;
+    }
+}
